f4d95cfa801964eaaef88c665f4049fc80084a33
shivam1057 <shivam.changil@apnibus.com>
2024-08-06 20:05:44 +0530

    refactor userdetailScreen

--- lib/ui/UserDetail/UserDetailScreen.dart
+++ lib/ui/UserDetail/UserDetailScreen.dart
@@ -6,7 +6,7 @@ import 'package:user_app/data/models/user_model.dart';
 class UserDetailScreen extends StatelessWidget {
   final User user;
 
-  UserDetailScreen({
+  const UserDetailScreen({
     Key? key,
     required this.user,
   }) : super(key: key);
@@ -21,37 +21,30 @@ class UserDetailScreen extends StatelessWidget {
           "User Detail",
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
-        actions: [IconButton(onPressed: sharePressed, icon: Icon(Icons.share))],
+        actions: [
+          IconButton(onPressed: _shareUserData, icon: Icon(Icons.share))
+        ],
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          _buildUserAvatar(context),
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: CircleAvatar(
-              backgroundColor: Colors.grey.shade100,
-              radius: MediaQuery.of(context).size.height * 0.1,
-              child: Image.asset(
-                "assets/user.png",
-                height: MediaQuery.of(context).size.height * 0.15,
-              ),
-            ),
-          ),
-          Text(
-            user.name ?? "",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            child: _buildUserName(),
           ),
           Expanded(
             child: ListView(
               physics: const BouncingScrollPhysics(),
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
               children: [
-                _buildInfoTile("UserName", user.username),
+                _buildInfoTile("Username", user.username),
                 _buildInfoTile("Email", user.email),
                 _buildInfoTile("Contact", user.phone),
-                _buildCompanyTile(user.company),
                 _buildInfoTile("Website",
                     user.website != null ? "www.${user.website}" : ""),
                 _buildAddressTile(user.address),
+                _buildCompanyTile(user.company),
               ],
             ),
           ),
@@ -60,24 +53,45 @@ class UserDetailScreen extends StatelessWidget {
     );
   }
 
+  Widget _buildUserAvatar(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: CircleAvatar(
+        backgroundColor: Colors.grey.shade100,
+        radius: MediaQuery.of(context).size.height * 0.1,
+        child: Image.asset(
+          "assets/user.png",
+          height: MediaQuery.of(context).size.height * 0.15,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserName() {
+    return Text(
+      user.name ?? "No Name",
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+    );
+  }
+
   Widget _buildInfoTile(String title, String? subtitle) {
     return ListTile(
-      contentPadding: const EdgeInsets.symmetric(vertical: 4.0),
+      contentPadding: const EdgeInsets.symmetric(vertical: 8.0),
       title: Text(
         title,
-        style: const TextStyle(
+        style: TextStyle(
           fontWeight: FontWeight.bold,
           color: Colors.black54,
         ),
       ),
-      subtitle: Text(subtitle ?? ""),
+      subtitle: Text(subtitle ?? "Not Available"),
     );
   }
 
   Widget _buildAddressTile(Address? address) {
-    if (address == null) return const SizedBox.shrink();
+    if (address == null) return SizedBox.shrink();
     return ListTile(
-      contentPadding: const EdgeInsets.symmetric(vertical: 4.0),
+      contentPadding: const EdgeInsets.symmetric(vertical: 8.0),
       title: const Text(
         "Address",
         style: TextStyle(
@@ -88,21 +102,21 @@ class UserDetailScreen extends StatelessWidget {
       subtitle: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Street: ${address.street ?? ''}"),
-          Text("Suite: ${address.suite ?? ''}"),
-          Text("City: ${address.city ?? ''}"),
-          Text("Zipcode: ${address.zipcode ?? ''}"),
+          Text("Street: ${address.street ?? 'Not Available'}"),
+          Text("Suite: ${address.suite ?? 'Not Available'}"),
+          Text("City: ${address.city ?? 'Not Available'}"),
+          Text("Zipcode: ${address.zipcode ?? 'Not Available'}"),
           Text(
-              "Geo: Lat: ${address.geo?.lat ?? ''}, Lng: ${address.geo?.lng ?? ''}"),
+              "Geo: Lat: ${address.geo?.lat ?? 'Not Available'}, Lng: ${address.geo?.lng ?? 'Not Available'}"),
         ],
       ),
     );
   }
 
   Widget _buildCompanyTile(Company? company) {
-    if (company == null) return const SizedBox.shrink();
+    if (company == null) return SizedBox.shrink();
     return ListTile(
-      contentPadding: const EdgeInsets.symmetric(vertical: 4.0),
+      contentPadding: const EdgeInsets.symmetric(vertical: 8.0),
       title: const Text(
         "Company",
         style: TextStyle(
@@ -113,34 +127,34 @@ class UserDetailScreen extends StatelessWidget {
       subtitle: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Name: ${company.name ?? ''}"),
-          Text("CatchPhrase: ${company.catchPhrase ?? ''}"),
-          Text("BS: ${company.bs ?? ''}"),
+          Text("Name: ${company.name ?? 'Not Available'}"),
+          Text("CatchPhrase: ${company.catchPhrase ?? 'Not Available'}"),
+          Text("BS: ${company.bs ?? 'Not Available'}"),
         ],
       ),
     );
   }
 
-  void sharePressed() {
+  void _shareUserData() {
     final String userData = '''
-  Name: ${user.name}
-  Username: ${user.username}
-  Email: ${user.email}
-  Address: 
-    Street: ${user.address?.street ?? ''}
-    Suite: ${user.address?.suite ?? ''}
-    City: ${user.address?.city ?? ''}
-    Zipcode: ${user.address?.zipcode ?? ''}
-    Geo: 
-      Lat: ${user.address?.geo?.lat ?? ''}
-      Lng: ${user.address?.geo?.lng ?? ''}
-  Phone: ${user.phone}
-  Website: ${user.website}
-  Company: 
-    Name: ${user.company?.name ?? ''}
-    CatchPhrase: ${user.company?.catchPhrase ?? ''}
-    BS: ${user.company?.bs ?? ''}
-  ''';
+Name: ${user.name ?? 'Not Available'}
+Username: ${user.username ?? 'Not Available'}
+Email: ${user.email ?? 'Not Available'}
+Address: 
+  Street: ${user.address?.street ?? 'Not Available'}
+  Suite: ${user.address?.suite ?? 'Not Available'}
+  City: ${user.address?.city ?? 'Not Available'}
+  Zipcode: ${user.address?.zipcode ?? 'Not Available'}
+  Geo: 
+    Lat: ${user.address?.geo?.lat ?? 'Not Available'}
+    Lng: ${user.address?.geo?.lng ?? 'Not Available'}
+Phone: ${user.phone ?? 'Not Available'}
+Website: ${user.website ?? 'Not Available'}
+Company: 
+  Name: ${user.company?.name ?? 'Not Available'}
+  CatchPhrase: ${user.company?.catchPhrase ?? 'Not Available'}
+  BS: ${user.company?.bs ?? 'Not Available'}
+''';
 
     Share.share(userData);
   }
